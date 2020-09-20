a19db0d8751454b0fa1f0452a53d089e76df6379
Abhilash Katuru <64161499+abhilash-katuru@users.noreply.github.com>
2020-09-20 12:32:57 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app."""
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -32,12 +32,15 @@ def after_request(response):
 
 conn = sqlite3.connect('data.db')
 
+
+
 try:
     conn.execute('''CREATE TABLE users
             (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
             username TEXT NOT NULL,
             password TEXT NOT NULL,
-            email TEXT NOT NULL);''')
+            email TEXT NOT NULL,
+            currency TEXT NOT NULL DEFAULT 'USD');''')
 except:
     #means table already exists
     pass
@@ -54,7 +57,53 @@ def index():
 @login_required
 def dashboard():
     conn = sqlite3.connect('data.db')
-    return render_template("dashboard.html")
+
+    rows = conn.execute('SELECT * FROM users WHERE id = ' +
+                        '"' + str(session["userid"]) + '"')
+    rows = rows.fetchall()
+
+    info = {'balance': convert(10000, session['curr']), 'bills': convert(
+        10000, session['curr']), 'food': convert(10000, session['curr']), 'shopping': convert(10000, session['curr']), 'savings': convert(10000, session['curr']), 'other': convert(10000, session['curr'])}
+
+    return render_template("dashboard.html", user = rows[0][1], info=info)
+
+#Settings page
+
+@app.route("/settings", methods=["GET", "POST"])
+@login_required
+def settings():
+    if request.method == "POST":
+        conn = sqlite3.connect('data.db')
+        if request.form.get('currency'):
+            command = 'UPDATE users SET currency = "' + request.form.get('currency') + '"' + ' WHERE id = ' + str(session['userid']) +';'
+            conn.execute(command)
+            conn.commit()
+            session['curr'] = convert_currency(request.form.get('currency'))
+            return render_template('settings.html', currency = session["curr"], saved = 'Saved!')
+        
+        elif request.form.get('current'):
+            
+            command = f"SELECT * FROM users WHERE id = " + str(session['userid']) + ';'
+            rows = conn.execute(command)
+            rows = rows.fetchall()
+            if not check_password_hash(rows[0][2], request.form.get('current')):
+                return render_template('settings.html', currency = session['curr'], invcur = 'Invalid current password!')
+            elif request.form.get('password') == request.form.get('current'):
+                return render_template('settings.html', currency = session['curr'], invnew = 'New password cannot be same as old password!')
+            elif not good_password(request.form.get('password')):
+                return render_template('settings.html', currency = session['curr'], invnew = 'You password must contain one uppercase character and a number!')
+            elif request.form.get('password') != request.form.get('confirmation'):
+                return render_template('settings.html', currency = session['curr'], invconf = 'New passwords must match!')
+            else:
+                command = 'UPDATE users SET password = "' + generate_password_hash(request.form.get('password')) + '" WHERE id = ' + str(session['userid']) + ';'
+                conn.execute(command)
+                conn.commit()
+                return render_template('settings.html', currency = session['curr'], savedpass = 'Saved!')
+
+
+    else:
+        return render_template('settings.html', currency = session["curr"])
+
 
 # Login page
 @app.route("/login", methods=["GET", "POST"])
@@ -90,8 +139,11 @@ def login():
         else:
             
             if check_password_hash(rows[0][2], key):
+
+                session["userid"] = rows[0][0]
+                session["curr"] = convert_currency(rows[0][4])
                 
-                return render_template('dashboard.html', user = rows[0][1])
+                return redirect('/dashboard')
             else:
                 
                 return render_template('login.html', invalid = 'Invalid Password!', default={'email': email})
@@ -141,14 +193,6 @@ def register():
         rows1 = conn.execute('SELECT * FROM users WHERE email = ' +
                             '"' + request.form.get('email') + '"')
         rows1 = rows1.fetchall()
-
-        #Lets not do this since we are loggin in with email, lets let them have whatever username they want
-        
-        # #Means that this username already exists
-        
-        # if len(rows) > 0:
-        
-        #     return render_template('register.html', invpas="Username already exists!")
         
         if len(rows1) > 0:
            
@@ -163,16 +207,18 @@ def register():
             
             conn.commit()
 
-            rows = rows = conn.execute(
-                'SELECT * FROM users WHERE username = ' + '"' + request.form.get('username') + '"')
+            rows = conn.execute(
+                'SELECT * FROM users WHERE email = ' + '"' + request.form.get('email') + '"')
             
             rows = rows.fetchall()
             
             session['userid'] = rows[0][0]
             
+            session['curr'] = convert_currency(rows[0][4])
+
             user = rows[0][1]
             
-            return render_template('dashboard.html', user = user)
+            return redirect('/dashboard')
         
 
 
@@ -181,5 +227,19 @@ def register():
         
         return render_template("register.html")
 
+@app.route('/quicklog', methods=['GET', 'POST'])
+def quicklog(): 
+    session.clear()
+    conn = sqlite3.connect('data.db')
+
+    rows = conn.execute('SELECT * FROM users WHERE email = ' +
+                        '"' + 'umsorrytest@gmail.com' + '"')
+    rows = rows.fetchall()
+    print(rows)
+    session["userid"] = rows[0][0]
+    session["curr"] = convert_currency(rows[0][4])
+    
+
+    return redirect('/dashboard')
 
 conn.commit()
--- helpers.py
+++ helpers.py
@@ -20,11 +20,26 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
+        if session.get("userid") is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
 
+
+def convert(num, curr):
+    return f'{curr} {num}'
+
+def convert_currency(t):
+    if t == "USD":
+        return '$'
+    if t == "IR":
+        return '₹'
+    if t == "BMK":
+        return 'K'
+    if t == 'BR':
+        return 'R$'
+    if t == "EUR":
+        return '€'
 
 def good_password(key):
     c1 = False
